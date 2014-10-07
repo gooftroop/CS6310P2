37aaade27dd11212926705bbd6b7b20c0113379e
gooftroop <bwebb@Bwebb-MBP.local>
2014-10-07 16:15:01 -0400

    should have the sphere creating now

--- src/simulation/Earth.java
+++ src/simulation/Earth.java
@@ -8,85 +8,141 @@ import simulation.util.GridCell;
 
 public final class Earth {
 	
-	// These two *were* found in an abstract class, but moved up here in case 
-	// we don't need that abstract class
-	public final float INITIAL_TEMP = 288;
+	private final float INITIAL_TEMP = 288;
 	
-	private GridCell root = null;
+	private final int DEFAULT_DEGREES = 15;
+	private final int DEFAULT_SPEED = 1; // minute
+	private final int MAX_DEGREES = 180;
+	private final int MAX_SPEED = 1440;
+	private final int SUN_START_POS = 0;
 	
-	private int height = 0, width = 0;
+	private final int[] increments = {6, 9, 10, 12, 15, 18, 20, 30, 36, 45, 60, 90, 180};
+	
+	private GridCell prime = null;
+	
+	private int sun;
+	private int speed = DEFAULT_SPEED;
+	private int gs = DEFAULT_DEGREES;
 	
 	public Earth() {
 		/* Empty */
 	}
 	
-	public Earth(int height, int width) {
+	public void configure(int gs, int speed) {
 		
-		this.setup(height, width);
-	}
-	
-	public void setup(int width, int height) {
+		if (gs <= 0 || gs > MAX_DEGREES)
+			throw new IllegalArgumentException("Invalid grid spacing");
 		
-		if (height < 0 || height >= Integer.MAX_VALUE)
-			throw new IllegalArgumentException("Invalid height dimension");
+		if (speed <= 0 || speed > MAX_SPEED)
+			throw new IllegalArgumentException("Invalid speed setting");
 		
-		if (width < 0 || width >= Integer.MAX_VALUE)
-			throw new IllegalArgumentException("Invalid width dimension");
+		this.speed = speed;
 		
-		this.height = height;
-		this.width = width;
-		
-		this.initializePlate();
+		// The following could be done better - if we have time, we should do so
+		if (MAX_DEGREES % gs != 0) {
+			for (int i = 1; i < increments.length; i++)
+				if (increments[i] > gs) this.gs = increments[i - 1];
+		} else this.gs = gs;
 	}
 
 	public void initializePlate() {
 		
-		int y = 0, x = 0;
+		int x = 0, y = 0;
 		
-		if (this.root != null) this.root.setTemp(INITIAL_TEMP);
-		else this.root = new GridCell(INITIAL_TEMP, x, y);
+		// do a reset
+		sun = SUN_START_POS;
 		
-		GridCell top = null, left = null, right = null, bottom = null, anchor = null, curr = root;
-		for (; x < this.width; x++) {
+		if (this.prime != null) this.prime.setTemp(INITIAL_TEMP);
+		else this.prime = new GridCell(INITIAL_TEMP, x, y);
+		this.prime.setTop(null);
+
+		/*
+		 * ok, so we start at the prime meridian (long. 0) and at the north pole (+90).
+		 * we construct the globe by working our way down from +90 to -90, and westward,
+		 * from 0 to 180.
+		 * 
+		 * The cells around each poles will have a bottom, left, and right, but no top (they are triangles)
+		 */
+		
+		// TODO We still need to set lat/long...any other attributes? for each cell
+		 
+		int spacing = (MAX_DEGREES / this.gs);
+		
+		// North Pole
+		GridCell next = null, curr = this.prime;
+		for (x = 0; x < spacing; x++) {
 			
-			y = 0;
+			if (curr.getLeft() != null) curr.getLeft().setTemp(INITIAL_TEMP);
+			else {
+				next = new GridCell(null, null, null, curr, INITIAL_TEMP, x, y);
+				curr.setLeft(next);
+			}
+			curr = curr.getLeft();
+		}
+		
+		// Stitch the grid row together
+		this.prime.setRight(curr);
+		curr.setLeft(this.prime);
+		
+		// Create each grid row, with the exception of the south pole
+		GridCell top = this.prime.getLeft(), left = null;
+		for (y = 1; y < spacing - 1; y++) {
 			
-			if (curr.getRight() != null) curr.getRight().setTemp(INITIAL_TEMP);
-			else curr.setRight(new GridCell(null, null, curr, null, INITIAL_TEMP, x, y));
-			
-			anchor = curr;
-			left = curr.getLeft();
-			top = curr.getRight();
-			
-			for (y = 1; x < this.height; y++) {
-				
-				if (curr.getBottom() != null) curr.getBottom().setTemp(INITIAL_TEMP);
-				else  {
-					
-					if (x != 0) {
-						left = left.getBottom();
-						bottom = new GridCell(curr, null, left, null, INITIAL_TEMP, x, y); 
-						left.setRight(bottom);
-					} else bottom = new GridCell(curr, null, null, null, INITIAL_TEMP, x, y); 
-					
-					curr.setBottom(bottom);
-				}
-				
-				if (x == this.width - 1) {
-					// right can never be null - otherwise we have an issue
-					right = right.getBottom();
-					curr.setRight(right);
-					right.setLeft(curr);
-				}
-				
-				curr = bottom;
+			if (top.getBottom() != null) { 
+				curr = top.getBottom();
+				curr.setTemp(INITIAL_TEMP);
+			} else {
+				curr = new GridCell(top, null, null, null, INITIAL_TEMP, 0, y);
+				top.setBottom(curr);
 			}
 			
-			curr.setBottom(anchor);
-			anchor.setTop(curr);
-			curr = top;
-			right = this.root;
+			for (x = 1; x < spacing; x++) {
+				
+				if (curr.getLeft() != null) curr.getLeft().setTemp(INITIAL_TEMP);
+				else {
+					next = new GridCell(top, null, null, curr, INITIAL_TEMP, x, y);
+					curr.setLeft(next);
+				}
+				
+				top = top.getLeft();
+				curr = curr.getLeft();
+			}
+			
+			left = top.getBottom(); // This should be the first cell we created
+			
+			// Stitch the grid row together
+			curr.setLeft(left);
+			left.setRight(curr);
+			top = left;
+			
 		}
+		
+		if (top.getBottom() != null) { 
+			curr = top.getBottom();
+			curr.setTemp(INITIAL_TEMP);
+		} else {
+			curr = new GridCell(top, null, null, null, INITIAL_TEMP, 0, y);
+			top.setBottom(curr);
+		}
+		
+		// South Pole
+		for (x = 1; x < MAX_DEGREES / this.gs; x++) {
+			
+			if (curr.getLeft() != null) { 
+				curr.getLeft().setTemp(INITIAL_TEMP);
+			} else {
+				next = new GridCell(top, null, null, curr, INITIAL_TEMP, x, y);
+				curr.setLeft(next);
+			}
+			curr = curr.getLeft();
+			top = top.getLeft();
+		}
+		
+		left = top.getBottom(); // This should be the first cell we created
+		
+		// Stitch the grid row together
+		curr.setLeft(left);
+		left.setRight(curr);
 	}
 	
 	public void run() {
@@ -96,8 +152,8 @@ public final class Earth {
 		
 		while(true) {
 			
-			bfs.add(this.root);
-			this.root.visited(true);
+			bfs.add(this.prime);
+			this.prime.visited(true);
 			
 			while(!bfs.isEmpty()) {
 				
@@ -124,8 +180,6 @@ public final class Earth {
 					child.calculateTemp();
 					bfs.add(child);
 				}
-				
-				// this.update(point.getTemp(), point.x, point.y); // We can use a ResultsHandler to update the grid to send...
 			}
 		}
 	}

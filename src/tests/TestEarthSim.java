410a723b32c11f1a12f55ac1f0031d24c8e921cf
ajing <ajingnk@gmail.com>
2014-10-13 16:12:04 -0400

    try to print from prime, some errors in initialization of grid

--- src/tests/TestEarthSim.java
+++ src/tests/TestEarthSim.java
@@ -9,8 +9,21 @@ public class TestEarthSim {
 		Earth earth = new Earth();
 		earth.configure(6, 10);
 		earth.initializePlate();
-		GridCell prime = earth.getGrid();
-		earth.run();
+		GridCell prime = Earth.getGrid();
+		//earth.run();
+		printGrid(prime);
 	}
-
+	
+	private static void printGrid(GridCell iniposition){
+		GridCell curr = iniposition;
+		int height = Earth.getHeight();
+		int width  = Earth.getWidth();
+		for (int x = 1; x < height - 1; x++) {
+			for (int y = 0; y < width; y++) {
+				GridCell rowgrid = curr.getRight();
+				System.out.println(rowgrid.getTemp());
+			}
+			curr = curr.getBottom();
+		}
+	}
 }
